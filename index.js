5c3caacae77987165979ea51033e424fe9660105
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-25 16:27:37 -0500

    Wrote playRound function

--- index.js
+++ index.js
@@ -22,6 +22,42 @@ function getComputerChoiceTester() {
     }
 }
 
+// A function that plays a single round given two choice items
+function playRound(playerSelection, computerSelection) {
+    let result;
+    // Same selection; draw case
+    if (playerSelection === computerSelection) {
+        result = "You both played " + playerSelection + " , this round is a draw";
+    } else {
+        let doesMyHandBeatsYours = myHandBeatsYours(playerSelection, computerSelection);
+        if (doesMyHandBeatsYours) {
+            result = "You played " + playerSelection + " and the computer played " + computerSelection + ". You win";
+        } else {
+            result = "You played " + playerSelection + " and the computer played " + computerSelection + ". You lose";
+        }
+    }
+
+    return result;
+
+}
+
+// A helper function that checks wheather a choice beats the other
+//  Does not check draws
+function myHandBeatsYours(choice1, choice2) {
+    // Check choice 1 in all three cases
+    let result = 1;
+    if (choice1 === choice[0] && choice2 === choice[1]) {
+        result = 0;
+    } else if (choice1 === choice[1] && choice2 === choice[2]) {
+        result = 0;
+    } else if (choice1 === choice[2] && choice2 === choice[0]) {
+        result = 0;
+    }
+
+    console.log(choice1, choice2);
+
+    return result;
+}
 
 
 
