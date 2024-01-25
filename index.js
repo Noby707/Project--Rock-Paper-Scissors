3af5ff192790716df479c81823badfc575a53d3b
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-25 16:03:24 -0500

    Added a function getComputerChoice and it's tester

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// User Interaction:
+//  Prompt User about game and rules
+//  User enters choice [rock, paper, scissors]
+//  User gets result for current round
+//  Repeat rounds till best out of three wins
+
+const choice = ["rock", "paper", "scissors"]; 
+
+function getComputerChoice() {
+    // A function that returns rock, paper. or scissors.
+    
+    // generateChoice will be randomly 0, 1, 2 and it will be an index for choice
+    let generateChoice = Math.floor(Math.random() * 3);
+
+    return choice[generateChoice];
+}
+
+
+function getComputerChoiceTester() {
+    // Tests that values will always be between 0, 1, and 2
+    for(let i = 0; i < 100; i++) {
+        console.log(getComputerChoice());
+    }
+}
+
+
+
+
+
 console.log("Hello Game!");
