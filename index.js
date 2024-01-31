be7b229f7770bbe41fe874356330534dcb89d643
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-01-31 08:53:20 -0500

    Removed Console Logic and added UI base code.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 //  Repeat rounds till best out of five wins
 
 const choice = ["rock", "paper", "scissors"]; 
+let score = [0, 0];
+
 
 // A function that returns rock, paper. or scissors.
 function getComputerChoice() {
@@ -14,16 +16,9 @@ function getComputerChoice() {
     return choice[generateChoice];
 }
 
-
-// Tests that values will always be between 0, 1, and 2
-function getComputerChoiceTester() {
-    for(let i = 0; i < 100; i++) {
-        console.log(getComputerChoice());
-    }
-}
-
 // A function that plays a single round given two choice items
 function playRound(playerSelection, computerSelection) {
+    updateLog(playerSelection, computerSelection);
     let result;
     // Same selection; draw case
     if (playerSelection === computerSelection) {
@@ -36,9 +31,7 @@ function playRound(playerSelection, computerSelection) {
             result = "You played " + playerSelection + " and the computer played " + computerSelection + ". You lose";
         }
     }
-
     return result;
-
 }
 
 // A helper function that checks wheather a choice beats the other
@@ -54,82 +47,50 @@ function myHandBeatsYours(choice1, choice2) {
         result = 0;
     }
 
-    console.log(choice1, choice2);
-
     return result;
 }
 
 
-// A game function to start the game
-
-function game() {
-    // Prompt User
-    console.log("Hello Game!");
-    console.log("This will be a game of rock, paper scissors. Best out of 5 rounds wins!");
-    
-    // Get playerSelection and Computer Selectoin
-    let playerSelection;
-    let computerSelection;
-    let score = [0, 0];
-
-    // Play five rounds
-    for (let i = 0; i < 5; i++) {
-        // Get Player and computer Selection
-        playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
-        computerSelection = getComputerChoice();
-
-        // Get result then update the score
-        let result = playRound(playerSelection, computerSelection);
-        score = updateScore(score, result);
-
-        // Show result and score
-        console.log(result);
-        showScore(score);
-    }
-    
-    // Report Winner
-    reportWinner(score);
-}
 
 
-// A helper function to show the winner
-function reportWinner(score) {
-    let report;
-    // Check if player score is larger than computer score
-    if (score[0] > score[1]) {
-        report = "Player wins!";
-    }
-    // Check if the scores are a draw
-    else if (score[0] == score[1]) {
-        report = "Tis' a draw!";
-    }
-    // Check if the computer score is larger than the player score
-    else {
-        report = "Computer wins";
-    }
-
-    console.log(report);
-}
-
-// A helper function to update the score
-function updateScore(score, result) {
-    // Check two cases, if player won or if player lost
-    let resultChar = result.charAt(result.length - 1);
-    if (resultChar === 'n') {
-        score[0]++;
-    } else if (resultChar === 'e') {
-        score[1]++;
-    }
-
-    return score;
-}
 
 
-// A helper function to show score
-function showScore(score) {
-    let text = "Score is " + score[0] + ", " + score[1] + ".";
-    console.log(text);
-}
 
-// Start the game
-// game();
+// Start of UI interaction
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+// Logs
+let logs = {}
+
+
+rockButton.addEventListener('click', (event) => {
+    console.log("Player Played Rock");
+    let result = playRound(choice[0], getComputerChoice());
+
+    console.log(result);
+});
+
+paperButton.addEventListener('click', () => {
+    console.log("Player Played Paper");
+    let result = playRound(choice[1], getComputerChoice());
+
+    console.log(result); 
+});
+
+scissorsButton.addEventListener('click', () => {
+    console.log("Player Played Scissors");
+    let result = playRound(choice[2], getComputerChoice());
+
+    console.log(result);
+});
+
+// EveryTime a round is played log the result
+let updateLog = (playerSelection, ComputerSelection) => {
+
+};
+
+
+let logStr = JSON.stringify(logs);
+console.log("Log: " + logStr);
